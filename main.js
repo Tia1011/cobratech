46d009a41166dcf96003634751c0ee2413fd2cd4
Leah Tailor <lt221@st-andrews.ac.uk>
2026-03-14 11:45:52 +0000

    create board

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ function rollDice(min, max) {
 }
 const rollDice6 = () => rollDice(1, 6);
 print (rollDice6);
+
+function createBoard(){
+    const matrixArray = createMatrix(n);
+    const board = document.querySelector('.main-board')
+    let str = "";
+    matrixArray.map(row => {
+        str += `
+            <div class="row">`
+        row.map(block => {
+            str += `
+                    <div 
+                      class="block ${block === 1 ? 'active' : ''} " data-value=${block}>
+                      ${block}
+                    </div>
+                `
+            })   
+        str += `</div>`
+    })
+    board.innerHTML = str;
+}
