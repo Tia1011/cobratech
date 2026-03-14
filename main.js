dc520ddc9d26265d665c69eeda23031d2d50ac9b
Srishti Grag <sg379@st-andrews.ac.uk>
2026-03-14 12:26:08 +0000

    questions

--- main.js
+++ main.js
@@ -35,5 +35,64 @@ function roll() {
     });
 }
 
+async function loadCSV(filePath) {
+  const response = await fetch(filePath);
+  const text = await response.text();
+
+  const rows = text.trim().split("\n");
+  const headers = rows[0].split(",");
+
+  const data = rows.slice(1).map(row => {
+    const values = row.split(",");
+    let obj = {};
+
+    headers.forEach((header, index) => {
+      obj[header] = values[index];
+    });
+
+    return obj;
+  });
+
+  return data;
+}
+
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+function getLevel1Questions(allQuestions) {
+
+  const categories = {
+    "Asset/Liability": [],
+    "Investments": [],
+    "Budgeting": [],
+    "Debt Management": []
+  };
+
+  // group by category
+  allQuestions.forEach(q => {
+    categories[q.Category].push(q);
+  });
+
+  let levelQuestions = [];
+
+  for (let category in categories) {
+    const randomFour = shuffle(categories[category]).slice(0, 4);
+    levelQuestions.push(...randomFour);
+  }
+
+  return shuffle(levelQuestions); // shuffle final 16
+}
+
+async function initGame() {
+
+  const questions = await loadCSV("data.csv");
+
+  const level1 = getLevel1Questions(questions);
+    console.log("ahsdgjsd");
+  console.log(level1); // 16 random questions
+  createBoard();
+}
+
+initGame();
 // Start the board
-createBoard();
